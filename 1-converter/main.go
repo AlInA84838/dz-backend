d71532225f848beaf83c01b0277baaa89719caeb
AlInA84838 <musihinaaliana@yandex.ru>
2025-05-10 19:23:23 +0300

    fix: исправление ошибок

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,69 +3,80 @@ package main
 import "fmt"
 
 func main() {
-	var startVal string
+	var from string
+	var to string
+
 	fmt.Printf("__ Конвертер валют __\n")
+	fmt.Println("Введите начальную валюту USD (u) / EUR (e) / RUB (r): ")
+	fmt.Scan(&from)
 
 	for {
-		fmt.Println("Введите начальную валюту USD (u) / EUR (e) / RUB (r): ")
-		fmt.Scan(&startVal)
 
-		if startVal != "e" && startVal != "r" && startVal != "u" {
+		if from != "e" && from != "r" && from != "u" {
 			fmt.Println("Валюта введена неправильно!")
 		} else {
-			result := inputValue(startVal)
-			fmt.Printf("Результат конвертации: %.2f\n", result)
+			result := inputValue()
+			if from == "u" {
+				fmt.Println("Введите итоговую валюту EUR (e) или RUB (r): ")
+			}
+			if from == "r" {
+				fmt.Println("Введите итоговую валюту EUR (e) или USD (u): ")
+			}
+			if from == "e" {
+				fmt.Println("Введите итоговую валюту USD (u) или RUB (r): ")
+			}
+
+			fmt.Scan(&to)
+			res := converter(from, to, result)
+			fmt.Printf("Результат конвертации: %.2f\n", res)
+			break
 		}
 
 	}
 }
 
-func inputValue(startVal string) float64 {
-	var endVal string
+func inputValue() float64 {
+
 	var amount float64
 
-	fmt.Print("Введите сумму: ")
-	fmt.Scan(&amount)
+	for {
+		fmt.Print("Введите сумму: ")
+		_, err := fmt.Scan(&amount)
+		if err != nil {
+			fmt.Println("Ошибка: введите число!")
 
-	fmt.Println("Введите итоговую валюту EUR (e) или RUB (r): ")
-	fmt.Scan(&endVal)
+			var discard string
+			fmt.Scanln(&discard)
+			continue
+		}
+		return amount
+	}
+
+}
+
+func converter(from, to string, amount float64) float64 {
+	const UandErate = 1.13
+	const RandUrate = 80.86
+
+	var result float64
 
 	switch {
-	case startVal == "u" && endVal == "e":
-		return USDtoEUR(amount)
-	case startVal == "u" && endVal == "r":
-		return USDtoRUB(amount)
-	case startVal == "e" && endVal == "u":
-		return EURtoUSD(amount)
-	case startVal == "e" && endVal == "r":
-		return EURtoUSD(USDtoRUB(amount))
-	case startVal == "r" && endVal == "e":
-		return USDtoRUB(USDtoEUR(amount))
-	case startVal == "r" && endVal == "u":
-		return RUBtoUSD(amount)
-
+	case from == "u" && to == "e":
+		result = amount / UandErate
+	case from == "u" && to == "r":
+		result = amount * RandUrate
+	case from == "e" && to == "u":
+		result = amount * UandErate
+	case from == "e" && to == "r":
+		result = amount * UandErate * RandUrate
+	case from == "r" && to == "e":
+		result = amount * RandUrate * UandErate
+	case from == "r" && to == "u":
+		result = amount * RandUrate
 	default:
 		fmt.Println("Ошибка: неверный ввод валют")
 		return 0
 	}
-}
 
-func USDtoEUR(amount float64) float64 {
-	const rate = 1.13
-	return amount / rate
-}
-
-func EURtoUSD(amount float64) float64 {
-	const rate = 1.13
-	return amount * rate
-}
-
-func USDtoRUB(amount float64) float64 {
-	const rate = 80.86
-	return amount * rate
-}
-
-func RUBtoUSD(amount float64) float64 {
-	const rate = 80.86
-	return amount / rate
+	return result
 }
